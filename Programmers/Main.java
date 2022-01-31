c40184bda00d5d9c599d4d3cc22b23c55fc1f721
PPPosik <eoanswkgksk@gmail.com>
2022-02-01 02:23:27 +0900

    Update 2022-02-01

--- Programmers/Main.java
+++ Programmers/Main.java
@@ -4,7 +4,11 @@ import Programmers.level2.Prob86052;
 
 public class Main {
     public static void main(String[] args) {
-        String[] grid = {"SL","LR"};
+        String[] grid = { "R", "R" };
         int[] answer = new Prob86052().solution(grid);
+
+        for (int n : answer) {
+            System.out.print(n + " ");
+        }
     }
 }
--- Programmers/level2/Prob86052.java
+++ Programmers/level2/Prob86052.java
@@ -1,8 +1,12 @@
 package Programmers.level2;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Prob86052 {
+    final int[] dy = { 0, 1, 0, -1 };
+    final int[] dx = { -1, 0, 1, 0 };
+
     HashSet<PosPair> path;
     char[][] map;
 
@@ -10,71 +14,82 @@ public class Prob86052 {
         path = new HashSet<PosPair>();
         map = new char[grid.length][];
 
-        for(int i = 0; i < map.length; i++) {
+        for (int i = 0; i < map.length; i++) {
             map[i] = grid[i].toCharArray();
         }
-        System.out.println(path.add(new PosPair(new Pos(0, -1), new Pos(0, 0))));
-        System.out.println(path.add(new PosPair(new Pos(0, -1), new Pos(0, 0))));
-        // System.out.println(move(new Pos(0, -1), new Pos(0, 0), 0));
-        // System.out.println(move(new Pos(1, 0), new Pos(0, 0), 0));
-        // System.out.println(move(new Pos(0, 1), new Pos(0, 0), 0));
-        // System.out.println(move(new Pos(-1, 0), new Pos(0, 0), 0));
 
-        return new int[3];
+        ArrayList<Integer> tmp = new ArrayList<Integer>();
+
+        for (int i = 0; i < dy.length; i++) {
+            int moveCnt = move(new Pos(dy[i], dx[i]), new Pos(0, 0), -1);
+            System.out.println("----------");
+            if (moveCnt > 0) {
+                tmp.add(moveCnt);
+            }
+        }
+
+        for (PosPair pair : path) {
+            System.out.println(pair.toString() + " " + pair.hashCode());
+        }
+
+        int[] answer = new int[tmp.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = tmp.get(i);
+        }
+
+        return answer;
     }
 
     private int move(Pos from, Pos to, int depth) {
-        if(!path.add(new PosPair(from, to))) {
+        if (depth > 10)
             return 0;
-        }
-        else if(depth != 0 && to.getY() == 0 && to.getX() == 0) {
+        if (!path.add(new PosPair(from, to))) {
             return depth;
         }
+        System.out.println(" (" + from.getY() + ", " + from.getX() + ") (" +
+                to.getY() + ", " + to.getX() + ") ");
 
-        Pos nextPos = null;
-        if(map[to.getY()][to.getX()] == 'S') {
+        Pos nextPos = new Pos(to.getY() < 0 ? map.length - 1 : to.getY() % map.length,
+                to.getX() < 0 ? map[0].length - 1 : to.getX() % map[0].length);
+        if (map[nextPos.getY()][nextPos.getX()] == 'S') {
             nextPos = straight(from, to);
-        }
-        else if(map[to.getY()][to.getX()] == 'L') {
+        } else if (map[nextPos.getY()][nextPos.getX()] == 'L') {
             nextPos = left(from, to);
-        }
-        else if(map[to.getY()][to.getX()] == 'R') {
+        } else if (map[nextPos.getY()][nextPos.getX()] == 'R') {
             nextPos = right(from, to);
         }
 
-        int ret = -1;
-        if(nextPos != null) {
-            ret = move(to, nextPos, depth + 1);
-        }
-
-        return ret;
+        return move(to, nextPos, depth + 1);
     }
 
     private Pos straight(Pos from, Pos to) {
         int dy = to.getY() - from.getY();
         int dx = to.getX() - from.getX();
-        int newY = (to.getY() + dy) % map.length;
-        int newX = (to.getX() + dx) % map[0].length;
 
-        return new Pos(newY < 0 ? map.length : newY, newX < 0 ? map[0].length : newX);
+        to.setY(to.getY() < 0 ? map.length - 1 : to.getY() % map.length);
+        to.setX(to.getX() < 0 ? map[0].length - 1 : to.getX() % map[0].length);
+
+        return new Pos(to.getY() + dy, to.getX() + dx);
     }
 
     private Pos left(Pos from, Pos to) {
         int dy = to.getY() - from.getY();
         int dx = to.getX() - from.getX();
-        int newY = (to.getY() + -1 * dx) % map.length;
-        int newX = (to.getX() + dy) % map[0].length;
-        
-        return new Pos(newY < 0 ? map.length : newY, newX < 0 ? map[0].length : newX);
+
+        to.setY(to.getY() < 0 ? map.length - 1 : to.getY() % map.length);
+        to.setX(to.getX() < 0 ? map[0].length - 1 : to.getX() % map[0].length);
+
+        return new Pos(to.getY() + -1 * dx, to.getX() + dy);
     }
-    
+
     private Pos right(Pos from, Pos to) {
         int dy = to.getY() - from.getY();
         int dx = to.getX() - from.getX();
-        int newY = (to.getY() + dx) % map.length;
-        int newX = (to.getX() + -1 * dy) % map[0].length;
-        
-        return new Pos(newY < 0 ? map.length : newY, newX < 0 ? map[0].length : newX);
+
+        to.setY(to.getY() < 0 ? map.length - 1 : to.getY() % map.length);
+        to.setX(to.getX() < 0 ? map[0].length - 1 : to.getX() % map[0].length);
+
+        return new Pos(to.getY() + dx, to.getX() + -1 * dy);
     }
 
     private class PosPair {
@@ -88,13 +103,18 @@ public class Prob86052 {
 
         @Override
         public boolean equals(Object obj) {
-            if(obj instanceof PosPair) {
-                // 바꿔야함
-                return this.toString().equals(obj.toString());
+            if (obj instanceof PosPair) {
+                PosPair tmp = (PosPair) obj;
+                // return true;
+                return tmp.first.equals(this.first) && tmp.second.equals(this.second);
             }
-            else {
-                return false;
-            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.toString().hashCode();
         }
 
         @Override
@@ -118,6 +138,30 @@ public class Prob86052 {
 
         public int getX() {
             return x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Pos) {
+                Pos tmp = (Pos) obj;
+                // return true;
+                return tmp.getY() == this.getY() && tmp.getX() == this.getX();
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.toString().hashCode();
         }
 
         @Override
