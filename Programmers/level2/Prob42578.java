5ce78388e28b740adb0af517270a6b65482c594c
PPPosik <eoanswkgksk@gmail.com>
2022-02-08 08:44:03 +0900

    Update 2022-02-08

--- Programmers/level2/Prob42578.java
+++ Programmers/level2/Prob42578.java
@@ -1,18 +1,13 @@
 package Programmers.level2;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
 public class Prob42578 {
-    HashMap<String, Integer> clothesTypeCnt;
-    ArrayList<String> types;
-
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
 
-        clothesTypeCnt = new HashMap<String, Integer>();
-        types = new ArrayList<String>();
+        HashMap<String, Integer> clothesTypeCnt = new HashMap<String, Integer>();
 
         for (String[] cloth : clothes) {
             Integer value = clothesTypeCnt.get(cloth[1]);
@@ -20,46 +15,9 @@ public class Prob42578 {
         }
 
         for (Entry<String, Integer> entry : clothesTypeCnt.entrySet()) {
-            types.add(entry.getKey());
+            answer *= entry.getValue() + 1;
         }
 
-        for (int depth = 0; depth < types.size(); depth++) {
-            boolean[] visited = new boolean[types.size()];
-            answer += getCombination(visited, 0, depth);
-            System.out.println(depth); // depth == 30 시간초과
-        }
-
-        return answer;
-    }
-
-    private int getCombination(boolean[] visited, int idx, int depth) {
-        if (depth < 0) {
-            ArrayList<String> combination = new ArrayList<String>();
-
-            for (int i = 0; i < visited.length; i++) {
-                if (visited[i]) {
-                    combination.add(types.get(i));
-                }
-            }
-
-            int ret = 1;
-            for (String str : combination) {
-                ret *= clothesTypeCnt.get(str) != null ? clothesTypeCnt.get(str) : 1;
-            }
-
-            return ret;
-        } else if (idx >= visited.length) {
-            return 0;
-        }
-
-        int ret = 0;
-
-        visited[idx] = true;
-        ret += getCombination(visited, idx + 1, depth - 1);
-
-        visited[idx] = false;
-        ret += getCombination(visited, idx + 1, depth);
-
-        return ret;
+        return answer - 1;
     }
 }
