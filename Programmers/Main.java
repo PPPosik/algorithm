0cef6a6016259280f4034363829123e209b896a0
PPPosik <eoanswkgksk@gmail.com>
2022-02-11 10:20:27 +0900

    Update 2022-02-11

--- Programmers/Main.java
+++ Programmers/Main.java
@@ -4,7 +4,7 @@ import Programmers.level2.Prob1844;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,0},{0,0,0,0,1}};
+        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
         int answer = new Prob1844().solution(maps);
 
         System.out.print("answer : " + answer);
--- Programmers/level2/Prob1844.java
+++ Programmers/level2/Prob1844.java
@@ -1,38 +1,60 @@
 package Programmers.level2;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Prob1844 {
     private final int[] dx = { 1, 0, -1, 0 };
     private final int[] dy = { 0, 1, 0, -1 };
-    private final int DEFAULT_VALUE = Integer.MAX_VALUE;
+    private final int DEFAULT_VALUE = -1;
 
     public int solution(int[][] maps) {
+        Queue<int[]> queue = new LinkedList<>();
         boolean[][] visited = new boolean[maps.length][maps[0].length];
-        int answer = dfs(maps, visited, 0, 0);
+        int[][] path = new int[maps.length][maps[0].length];
 
-        return answer == DEFAULT_VALUE ? -1 : answer;
-    }
-
-    private int dfs(int[][] maps, boolean[][] visited, int y, int x) {
-        if (y == maps.length - 1 && x == maps[0].length - 1) {
-            return 1;
+        for (int[] arr : path) {
+            Arrays.fill(arr, DEFAULT_VALUE);
         }
-        visited[y][x] = true;
 
-        int ret = DEFAULT_VALUE;
+        int[] startPos = { 0, 0 };
+        path[startPos[0]][startPos[1]] = 1;
+        visited[startPos[0]][startPos[1]] = true;
+        queue.offer(startPos);
 
-        for (int i = 0; i < dy.length; i++) {
-            int newY = y + dy[i];
-            int newX = x + dx[i];
+        while (!queue.isEmpty()) {
+            int[] pos = queue.poll();
 
-            if (0 <= newY && newY < maps.length && 0 <= newX && newX < maps.length) {
-                if (maps[newY][newX] == 1 && !visited[newY][newX]) {
-                    ret = Math.min(ret, dfs(maps, visited, newY, newX));
-                    visited[newY][newX] = false;
-                    System.out.println("(" + newY + ", " + newX + ") " + ret);
+            for (int i = 0; i < dy.length; i++) {
+                int newY = pos[0] + dy[i];
+                int newX = pos[1] + dx[i];
+
+                if (0 <= newY && newY < maps.length && 0 <= newX && newX < maps[0].length) {
+                    if (maps[newY][newX] == 1 && !visited[newY][newX]) {
+                        if (path[newY][newX] == DEFAULT_VALUE || path[pos[0]][pos[1]] + 1 < path[newY][newX]) {
+                            int[] tmp = { newY, newX };
+
+                            path[newY][newX] = path[pos[0]][pos[1]] + 1;
+                            visited[newY][newX] = true;
+                            queue.offer(tmp);
+                        }
+                    }
                 }
             }
         }
 
-        return ret == DEFAULT_VALUE ? ret : ret + 1;
+        for (int[] aa : path) {
+            for (int a : aa) {
+                if (a < 0 || a > 9) {
+                    System.out.print(a + " ");
+                } else {
+                    System.out.print(" " + a + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        return path[path.length - 1][path[0].length - 1];
     }
 }
