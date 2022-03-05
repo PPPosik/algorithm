b21895fdc1b5e8cc57b72e496c97ee4192900872
PPPosik <eoanswkgksk@gmail.com>
2022-03-06 00:34:49 +0900

    Update 2022-03-06

--- Programmers/level2/Prob42583.java
+++ Programmers/level2/Prob42583.java
@@ -6,36 +6,35 @@ import java.util.Queue;
 public class Prob42583 {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> queue = new LinkedList<>();
-        int[] queueSize = new int[truck_weights.length];
         int nowWeight = 0;
         int answer = 0;
 
-        for (int i = 0; i < truck_weights.length; i++) {
-            boolean isPolled = false;
+        for (int i = 0; i < bridge_length; i++) {
+            queue.offer(0);
+        }
 
-            if (nowWeight + truck_weights[i] > weight || queue.size() > bridge_length) {
-                isPolled = true;
-                int idx = queue.poll();
-                System.out.print("poll " + truck_weights[idx]);
-                nowWeight -= truck_weights[idx];
-                answer += bridge_length - queueSize[idx];
-                System.out.println(" " + answer);
+        int idx = 0;
+        while (idx < truck_weights.length) {
+            nowWeight -= queue.poll();
+
+            if (nowWeight + truck_weights[idx] <= weight) {
+                queue.offer(truck_weights[idx]);
+                nowWeight += truck_weights[idx];
+                idx++;
+            } else {
+                queue.offer(0);
             }
 
-            if (nowWeight + truck_weights[i] <= weight && queue.size() <= bridge_length) {
-                queueSize[i] = queue.size();
-                queue.offer(i);
-                nowWeight += truck_weights[i];
+            answer++;
+        }
 
-                if (!isPolled) {
-                    answer++;
-                }
-                System.out.println("offer " + i + " " + answer);
+        int dequeueCnt = 0;
+        for (int i = 0; !queue.isEmpty(); i++) {
+            if (queue.poll() != 0) {
+                dequeueCnt = i + 1;
             }
         }
 
-        answer += bridge_length - queueSize[queueSize.length - queue.size()];
-
-        return answer;
+        return answer + dequeueCnt;
     }
 }
