b45a132dca2a89946c5488bb3fc7e2a4abcd0e7e
PPPosik <eoanswkgksk@gmail.com>
2022-05-04 22:34:00 +0900

    Update 2022-05-04

--- Programmers/level3/Prob72415.java
+++ Programmers/level3/Prob72415.java
@@ -9,78 +9,124 @@ public class Prob72415 {
     final int[] dx = { 1, 0, -1, 0 };
 
     public int solution(int[][] board, int r, int c) {
-        int answer = board[r][c] != 0 ? 1 : 0;
+        int size = getSize(board);
+        int[] cardNums = new int[size];
+        boolean[] visited = new boolean[size];
+        int answer = 0;
 
-        while (true) {
-            int[] pos = getNextPos(board, r, c);
-            if (pos[0] == -1 && pos[1] == -1) {
-                break;
+        answer += getEnterCnt(board);
+        answer += permutation(board, r, c, cardNums, visited, 0);
+
+        return answer;
+    }
+
+    private int getEnterCnt(int[][] board) {
+        int cnt = 0;
+
+        for (int[] arr : board) {
+            for (int n : arr) {
+                if (n != 0) {
+                    cnt++;
+                }
             }
-            answer += getMoveCnt(board, r, c, pos[0], pos[1]);
+        }
 
-            eraseBoard(board, r, c, pos[0], pos[1]);
+        return cnt;
+    }
 
-            r = pos[0];
-            c = pos[1];
+    private int getSize(int[][] board) {
+        int size = 0;
+
+        for (int[] arr : board) {
+            for (int n : arr) {
+                size = Math.max(size, n);
+            }
+        }
+
+        return size;
+    }
+
+    private int permutation(int[][] board, int y, int x, int[] cardNums, boolean[] visited, int depth) {
+        if (depth == cardNums.length) {
+            return getAnswer(board, y, x, cardNums);
+        }
+
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < cardNums.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                cardNums[depth] = i + 1;
+                answer = Math.min(answer, permutation(board, y, x, cardNums, visited, depth + 1));
+                visited[i] = false;
+            }
         }
 
         return answer;
     }
 
-    private int[] getNextPos(int[][] board, int y, int x) {
-        int[] pos = new int[] { -1, -1 };
+    private int getAnswer(int[][] board, int y, int x, int[] cardNums) {
+        int[][] map = new int[4][4];
+        int[][] dp = new int[cardNums.length + 1][2];
+        int[][][] pos = new int[cardNums.length + 1][2][2];
+        boolean flag = true;
 
-        if (board[y][x] != 0) {
-            for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board[0].length; j++) {
-                    if (board[i][j] == board[y][x] && !(y == i && x == j)) {
-                        pos[0] = i;
-                        pos[1] = j;
-                    }
-                }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                map[i][j] = board[i][j];
             }
-        } else {
-            Queue<int[]> queue = new LinkedList<>();
-            boolean[][] visited = new boolean[board.length][board[0].length];
+        }
 
-            queue.offer(new int[] { y, x });
-            visited[y][x] = true;
+        pos[0][0][0] = y;
+        pos[0][0][1] = x;
+        pos[0][1][0] = y;
+        pos[0][1][1] = x;
 
-            while (!queue.isEmpty()) {
-                int[] p = queue.poll();
-
-                if (board[p[0]][p[1]] != 0) {
-                    pos = p;
-                    break;
-                }
-
-                for (int i = 0; i < 4; i++) {
-                    int newY = p[0] + dy[i];
-                    int newX = p[1] + dx[i];
-
-                    if (0 <= newY && newY < board.length && 0 <= newX && newX < board[0].length
-                            && !visited[newY][newX]) {
-                        visited[newY][newX] = true;
-                        queue.offer(new int[] { newY, newX });
+        for (int k = 0; k < cardNums.length; k++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
+                    if (map[i][j] == cardNums[k]) {
+                        if (flag) {
+                            pos[k + 1][0][0] = i;
+                            pos[k + 1][0][1] = j;
+                        } else {
+                            pos[k + 1][1][0] = i;
+                            pos[k + 1][1][1] = j;
+                        }
+                        flag = !flag;
                     }
                 }
             }
         }
 
-        return pos;
+        for (int i = 1; i < pos.length; i++) {
+            dp[i][0] = Math.min(
+                    dp[i - 1][0] + getMoveCnt(map, pos[i - 1][0], pos[i - 1][1])
+                            + getMoveCnt(map, pos[i - 1][1], pos[i][0]),
+                    dp[i - 1][1] + getMoveCnt(map, pos[i - 1][1], pos[i - 1][0])
+                            + getMoveCnt(map, pos[i - 1][0], pos[i][0]));
+            dp[i][1] = Math.min(
+                    dp[i - 1][0] + getMoveCnt(map, pos[i - 1][0], pos[i - 1][1])
+                            + getMoveCnt(map, pos[i - 1][1], pos[i][1]),
+                    dp[i - 1][1] + getMoveCnt(map, pos[i - 1][1], pos[i - 1][0])
+                            + getMoveCnt(map, pos[i - 1][0], pos[i][1]));
+            eraseBoard(map, cardNums[i - 1]);
+        }
+
+        return Math.min(dp[cardNums.length][0] + getMoveCnt(map, pos[cardNums.length][0], pos[cardNums.length][1]),
+                dp[cardNums.length][1] + getMoveCnt(map, pos[cardNums.length][1], pos[cardNums.length][0]));
     }
 
-    private int getMoveCnt(int[][] board, int beforeY, int beforeX, int afterY, int afterX) {
-        int cnt = 1;
+    private int getMoveCnt(int[][] board, int[] pos, int[] targetPos) {
+        int cnt = 0;
 
         int[][] dp = new int[4][4];
         for (int[] arr : dp) {
             Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        dp[beforeY][beforeX] = 0;
+        dp[pos[0]][pos[1]] = 0;
 
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] { beforeY, beforeX });
+        queue.offer(new int[] { pos[0], pos[1] });
 
         while (!queue.isEmpty()) {
             int[] p = queue.poll();
@@ -119,15 +165,18 @@ public class Prob72415 {
             }
         }
 
-        cnt += dp[afterY][afterX];
+        cnt += dp[targetPos[0]][targetPos[1]];
 
         return cnt;
     }
 
-    private void eraseBoard(int[][] board, int beforeY, int beforeX, int afterY, int afterX) {
-        if (board[beforeY][beforeX] != 0 && board[beforeY][beforeX] == board[afterY][afterX]) {
-            board[beforeY][beforeX] = 0;
-            board[afterY][afterX] = 0;
+    private void eraseBoard(int[][] board, int target) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (board[i][j] == target) {
+                    board[i][j] = 0;
+                }
+            }
         }
     }
 }
