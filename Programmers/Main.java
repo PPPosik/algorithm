dd9406127bb6773bdc74a7c3548a40827fb7f0b5
PPPosik <eoanswkgksk@gmail.com>
2022-05-13 09:59:19 +0900

    Update 2022-05-13

--- Programmers/Main.java
+++ Programmers/Main.java
@@ -4,13 +4,8 @@ import Programmers.level3.*;
 
 public class Main {
     public static void main(String[] args) {
-        String[] operations = {"I 7","I 5","I -5","D -1"};
+        int answer = new Prob12900().solution(4);
 
-        int[] answer = new Prob42628().solution(operations);
-
-        System.out.print("answer : ");
-        for(int n : answer) {
-            System.out.print(n + " ");
-        }
+        System.out.print("answer : " + answer);
     }
 }
---
+++ Programmers/level3/Prob12900.java
@@ -0,0 +1,16 @@
+package Programmers.level3;
+
+public class Prob12900 {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
